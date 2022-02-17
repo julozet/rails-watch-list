f51c6523590bb0ad84b8e4ce03075abde3994091
julien lozet <lozetjulien@gmail.com>
2022-02-17 19:57:04 +0100

    adding css to show page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,9 @@
 class BookmarksController < ApplicationController
 
-
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
-    raise
     if @bookmark.save
       redirect_to list_path(@list)
     else
