00aaedea0ac568d2277fced3a25488ea45e7e5c2
julien lozet <lozetjulien@gmail.com>
2022-02-17 12:11:58 +0100

    add validations & links between tables

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, :movie_id, :list_id, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list}
 end
