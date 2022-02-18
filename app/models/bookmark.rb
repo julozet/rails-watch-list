8a3146feafd10c642adf2c5c5286f78340a95b4f
julien lozet <lozetjulien@gmail.com>
2022-02-18 11:31:48 +0100

    save before deploy

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,5 +4,5 @@ class Bookmark < ApplicationRecord
 
   validates :comment, :movie_id, :list_id, presence: true
   validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list}
+  validates :movie_id, uniqueness: { scope: :list_id}
 end
